612b8931a3646a2ed8459a51dc7537587ce461e4
rrTio <tio.rozsraven@ue.edu.ph>
2024-03-30 22:02:06 +0800

    Updated Portfolio

---
+++ assets/js/portfolio.js
@@ -0,0 +1,19 @@
+function bodyDefense(){
+    window.open("https://github.com/rrTio/BODY-DEFENSE-PC", '_blank')
+}
+
+function bms(){
+    window.open("https://github.com/rrTio/NCP3106-FINALS-Barangay-Management-System", '_blank')
+}
+
+function phpmsp(){
+    window.open("https://github.com/rrTio/data-manager", '_blank')
+}
+
+function psql(){
+    window.open("https://github.com/rrTio/PYTHON-REGISTRATION-PROJECT", '_blank')
+}
+
+function phpencdec(){
+    window.open("https://github.com/rrTio/PHP-Encryption-Decryption", '_blank')
+}
